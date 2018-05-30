fc2a6e904adf5d9ad5b5750f7e7722b0589aee33
cjrt27 <cameron.jr.thorn@gmail.com>
2018-05-30 16:43:34 +0100

    sorting out content page for formatting of home page

--- themes/quotesondev-theme/template-parts/content.php
+++ themes/quotesondev-theme/template-parts/content.php
@@ -5,14 +5,33 @@
  * @package QOD_Starter_Theme
  */
 
+$source = get_post_meta(get_the_ID(), '_qod_quote_source',true);
+$source_url= get_post_meta(get_the_ID(), '_qod_quote_source_url',true);
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
+<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+	<div class="entry-metat">
+		<?php the_title('<h2 class="entry-title">', '</h2>') ; ?>
+
+		<?php if($source && $source_url) : ?>
+			<span class="source">, <a href="<?php $source_url; ?>"><?php echo $source;?></a></span>
+		<?php elseif($source) : ?>
+			<span class="source"> <?php echo $source; ?></span>
+		<?php else : ?>
+			<span class="source"></span>
+		<?php endif ?>
+	</div>
+
+	
+</article>
+
+<!-- Button for home page and single page -->
+
+<?php if (is_home() || is_single() ) : ?>
+	<button type="button" id="new-quote-button">Show Me Another!</button>
+	<?php endif ?>
+ 
